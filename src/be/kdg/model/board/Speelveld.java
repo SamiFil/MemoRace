9afd65a7f0b313426018bf3c7eb2ff50e960eeb7
Sami <sami.filjak@student.kdg.be>
2023-02-27 01:54:42 +0100

    nieuwer

--- src/be/kdg/model/board/Speelveld.java
+++ src/be/kdg/model/board/Speelveld.java
@@ -5,58 +5,95 @@ import java.util.Collections;
 import java.util.List;
 
 public class Speelveld {
-    private final int dimensie;
-    private final Card[][] kaarten;
-    private Card geselecteerdeKaart;
+    private int rows;
+    private int columns;
+    private Card[][] kaarten;
+    private int aantalOmgedraaid;
+    private Card kaart1;
+    private Card kaart2;
 
-    public Speelveld(int dimensie) {
-        this.dimensie = dimensie;
-        this.kaarten = new Card[dimensie][dimensie];
-        vulSpeelveld();
+    public Speelveld(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        kaarten = new Card[rows][columns];
+        aantalOmgedraaid = 0;
     }
 
-    private void vulSpeelveld() {
-        List<Integer> waardes = new ArrayList<>();
-        for (int i = 0; i < dimensie * dimensie / 2; i++) {
-            waardes.add(i);
-            waardes.add(i);
-        }
-        Collections.shuffle(waardes);
-        for (int i = 0; i < dimensie; i++) {
-            for (int j = 0; j < dimensie; j++) {
-                int waarde = waardes.remove(0);
-                kaarten[i][j] = new Card(waarde);
-            }
-        }
+    public int getAantalOmgedraaid() {
+        return aantalOmgedraaid;
     }
 
-    public Card getKaart(int rij, int kolom) {
-        return kaarten[rij][kolom];
+    public void setAantalOmgedraaid(int aantalOmgedraaid) {
+        this.aantalOmgedraaid = aantalOmgedraaid;
     }
 
-    public void draaiKaartOm(Card kaart) {
-        if (geselecteerdeKaart == null) {
-            geselecteerdeKaart = kaart;
-        } else {
-            if (geselecteerdeKaart.getWaarde() == kaart.getWaarde()) {
-                geselecteerdeKaart.setGelijk(true);
-                kaart.setGelijk(true);
-            }
-            geselecteerdeKaart.setOmgedraaid(false);
-            kaart.setOmgedraaid(false);
-            geselecteerdeKaart = null;
-        }
-        kaart.setOmgedraaid(true);
+    public Card getKaart(int row, int column) {
+        return kaarten[row][column];
+    }
+
+    public void setKaart(int row, int column, Card kaart) {
+        kaarten[row][column] = kaart;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public String getWaarde(int row, int column) {
+        return kaarten[row][column].getWaarde();
+    }
+
+    public void setGelijk(int row1, int column1, int row2, int column2) {
+        kaarten[row1][column1].setGelijk(true);
+        kaarten[row2][column2].setGelijk(true);
+    }
+
+    public void setOmgedraaid(int row, int column, boolean omgedraaid) {
+        kaarten[row][column].setOmgedraaid(omgedraaid);
+    }
+
+    public boolean isOmgedraaid(int row, int column) {
+        return kaarten[row][column].isOmgedraaid();
+    }
+
+    public boolean isGelijk(int row1, int column1, int row2, int column2) {
+        return kaarten[row1][column1].getWaarde().equals(kaarten[row2][column2].getWaarde());
     }
 
     public boolean isGewonnen() {
-        for (int i = 0; i < dimensie; i++) {
-            for (int j = 0; j < dimensie; j++) {
-                if (!kaarten[i][j].isGelijk()) {
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                if (!kaarten[row][column].isGelijk()) {
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    public int getRij(Card kaart) {
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                if (kaarten[row][column] == kaart) {
+                    return row;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int getKolom(Card kaart) {
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                if (kaarten[row][column] == kaart) {
+                    return column;
+                }
+            }
+        }
+        return -1;
     }
 }
